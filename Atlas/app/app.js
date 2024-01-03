80f79d0914d827134ebd6334e79e4cbb1a591441
Captain-Oski <Captain-Oski@users.noreply.github.com>
2024-01-03 19:49:20 +0000

    update atlas stack

---
+++ Atlas/app/app.js
@@ -0,0 +1,8 @@
+// création de la carte Mapbox GL
+var map = new maplibregl.Map({
+    container: 'map', // identifiant de l'élément HTML conteneur de la carte
+    style: 'https://api.maptiler.com/maps/dataviz/style.json?key=JhO9AmIPH59xnAn5GiSj', // URL du style de la carte
+    center: [-73.55, 45.55], // position centrale de la carte
+    zoom: 9, // niveau de zoom initial
+    hash: true // activation du hash pour la gestion de l'historique de la carte
+});
