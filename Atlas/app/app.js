6387444935b2d5e3e07f050710aca125a2b0edef
Captain-Oski <Captain-Oski@users.noreply.github.com>
2024-03-27 13:43:43 +0000

    update TP3 template

--- Atlas/app/app.js
+++ Atlas/app/app.js
@@ -6,3 +6,29 @@ var map = new maplibregl.Map({
     zoom: 9, // niveau de zoom initial
     hash: true // activation du hash pour la gestion de l'historique de la carte
 });
+
+
+function loadTeam(teamName) {
+    // Vider toutes les divs
+    document.getElementById('Equipe1').innerHTML = '';
+    document.getElementById('Equipe2').innerHTML = '';
+    document.getElementById('Equipe3').innerHTML = '';
+    document.getElementById('Equipe4').innerHTML = '';
+    document.getElementById('Equipe5').innerHTML = '';
+    document.getElementById('Equipe6').innerHTML = '';
+    document.getElementById('Equipe7').innerHTML = '';
+    // Charger le contenu de l'équipe sélectionnée
+    fetch('./' + teamName + '/index.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(teamName).innerHTML = data;
+        })
+        .catch(error => {
+            console.error('There was a problem with fetching the team content:', error);
+        });
+}
