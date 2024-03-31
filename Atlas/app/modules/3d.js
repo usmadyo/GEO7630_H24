6d074b023ebd7112976118ceba0b6ef281f65f38
Captain-Oski <Captain-Oski@users.noreply.github.com>
2024-03-31 21:22:06 +0000

    udpate 3d comments

--- Atlas/app/modules/3d.js
+++ Atlas/app/modules/3d.js
@@ -1,18 +1,30 @@
-function generate3D(){
-    console.log('test')
-    removeAllLayersAndSources()
-    var grid = makeGrid()
-    console.log(grid)
+/**
+ * Fonction pour générer une représentation 3D d'une grille sur une carte
+ */
+function generate3D() {
+    // Affiche un message de test dans la console
+    console.log('test');
 
+    // Supprime toutes les couches et sources existantes de la carte
+    removeAllLayersAndSources();
+
+    // Crée une grille en utilisant la fonction makeGrid()
+    var grid = makeGrid();
+    console.log(grid);
+
+    // Ajoute une nouvelle source de données geojson à la carte avec la grille générée
     map.addSource('3d-source', {
         type: 'geojson',
         data: grid
     });
+
+    // Ajoute une nouvelle couche de type fill-extrusion à la carte pour afficher la grille en 3D
     map.addLayer({
         'id': '3dgrip',
         'source': '3d-source',
         'type': 'fill-extrusion',
         'paint': {
+            // Définit la couleur de remplissage de l'extrusion en fonction de la valeur aléatoire 'randomValue'
             'fill-extrusion-color': [
                 'interpolate', ['linear'], ['get', 'randomValue'],
                 0, '#f3e79b',
@@ -21,16 +33,19 @@ function generate3D(){
                 80, '#ce6693',
                 100, '#a059a0'
             ],
+            // Définit la hauteur de l'extrusion en fonction de la valeur aléatoire 'randomValue'
             'fill-extrusion-height': [
                 'interpolate', ['linear'], ['get', 'randomValue'],
                 0, 100,
                 100, 4000
             ],
+            // Définit l'opacité de l'extrusion
             'fill-extrusion-opacity': 0.6
         }
     });
 }
 
+// Ajoute un événement d'écoute du clic sur l'élément HTML avec l'ID 'generate3D'
 document
-  .getElementById('generate3D')
-  .addEventListener('click', generate3D);
+    .getElementById('generate3D')
+    .addEventListener('click', generate3D);
