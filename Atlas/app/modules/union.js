d569e0548e0e733b6eb652b4eab2b13a73678a1f
Captain-Oski <Captain-Oski@users.noreply.github.com>
2024-04-01 20:07:23 +0000

    update partie 3

--- Atlas/app/modules/union.js
+++ Atlas/app/modules/union.js
@@ -1,78 +1,37 @@
-/**
- * Cette fonction prend en entrée une FeatureCollection
- * et renvoie une Feature qui représente la fusion de toutes les Features dans la collection
- * Elle utilise la fonction Turf.js "dissolve" pour dissoudre les limites entre les features adjacentes.
- * @param {FeatureCollection} featureCollection - un set de points surlesquels on travaille
- * @returns {FeatureCollection} Une featureCollection de polygone dissous
- */
-function unionFeatures(featureCollection) {
-    const mergedFeature = turf.dissolve(featureCollection);
-    return mergedFeature;
+
+function generateGrid () {
+
+    // Définir les coordonnées de la zone d'étude
+    const bbox = [-73.990959, 45.410154, -73.467327, 45.705839];
+    // Définir la taille des cellules
+    const cellSide = document.getElementById('hexagonRadius').value ;
+    // Définir les options de l'objet de la grille
+    const options = { units: 'meters' };
+    // Créer la grille hexagonale
+    const hexgrid = turf.hexGrid(bbox, cellSide, options);
+    // Ajouter les données de la grille à la carte
+
+    map.addSource('grid-source', {
+        type: "geojson",
+        data: hexgrid
+    })
+    // Ajouter la couche à la carte
+    map.addLayer({
+        id: 'grid',
+        type: 'line',
+        source: 'grid-source',
+        paint: {
+            "line-opacity": 0.25
+        }
+    })
 }
 
-/**
-Cette fonction gère les trois différentes options de l'interface utilisateur
-en fonction de l'option sélectionnée.
-Si la première option est sélectionnée, elle calcule et affiche l'union des cercles générés aléatoirement.
-Si la deuxième option est sélectionnée, elle calcule et affiche une grille hexagonale.
-Si la troisième option est sélectionnée, elle calcule et affiche une jointure spatiale entre les points générés aléatoirement et la grille hexagonale.
-*/
 function union() {
-    // Vérifier si l'option d'union a été sélectionnée
-    if (document.getElementById('inputGroupSelect03').value == 1) {
-        // Récupérer le rayon sélectionné par l'utilisateur
-        const radiusInput = document.getElementById('radiusInput').value
-        // Créer un buffer pour chaque point généré aléatoirement
-        const buffers = createBuffer(randomPoints, radiusInput)
-        // Fusionner les buffers créés
-        const unionized = unionFeatures(buffers)
-        // Ajouter les données fusionnées à la carte
-        map.addSource('union-source', {
-            type: "geojson",
-            data: unionized
-        })
-        // Ajouter la couche à la carte
-        map.addLayer({
-            id: 'union',
-            type: 'fill',
-            source: 'union-source',
-            paint: {
-                'fill-color': 'orange',
-                'fill-opacity': 0.5
-            }
-        })
-    }
-    // Vérifier si l'option de la grille hexagonale a été sélectionnée
-    if (document.getElementById('inputGroupSelect03').value == 2) {
+    console.log('test')
         // Définir les coordonnées de la zone d'étude
         const bbox = [-73.990959, 45.410154, -73.467327, 45.705839];
         // Définir la taille des cellules
-        const cellSide = 500;
-        // Définir les options de l'objet de la grille
-        const options = { units: 'meters' };
-        // Créer la grille hexagonale
-        const hexgrid = turf.hexGrid(bbox, cellSide, options);
-        // Ajouter les données de la grille à la carte
-        map.addSource('grid-source', {
-            type: "geojson",
-            data: hexgrid
-        })
-        // Ajouter la couche à la carte
-        map.addLayer({
-            id: 'grid',
-            type: 'line',
-            source: 'grid-source',
-            paint: {
-                "line-opacity": 0.2
-            }
-        })
-    }
-    // Vérifier si l'option de jointure spatiale a été sélectionnée
-    if (document.getElementById('inputGroupSelect03').value == 3) {
-        // Définir les coordonnées de la zone d'étude
-        const bbox = [-73.990959, 45.410154, -73.467327, 45.705839];
-        // Définir la taille des cellules
-        const cellSide = 500;
+        const cellSide = document.getElementById('hexagonRadius').value ;
         // Définir les options de l'objet de la grille
         const options = { units: 'meters' };
         // Créer la grille hexagonale
@@ -81,7 +40,7 @@ function union() {
         const joined = countPointsInPolygons(randomPoints, hexgrid)
         map.addSource('grid-source', {
             type: "geojson",
-            data: countPointsInPolygons(randomPoints, hexgrid)
+            data: joined
   
         })
   
@@ -99,9 +58,55 @@ function union() {
             }
         })
     }
+
+function dissolver () {
+     // Récupérer le rayon sélectionné par l'utilisateur
+     const radiusInput = document.getElementById('radiusInput').value
+     // Créer un buffer pour chaque point généré aléatoirement
+     const buffers = createBuffer(randomPoints, radiusInput)
+     // Fusionner les buffers créés
+     const unionized = unionFeatures(buffers)
+     // Ajouter les données fusionnées à la carte
+     map.addSource('union-source', {
+         type: "geojson",
+         data: unionized
+     })
+     // Ajouter la couche à la carte
+     map.addLayer({
+         id: 'union',
+         type: 'fill',
+         source: 'union-source',
+         paint: {
+             'fill-color': 'orange',
+             'fill-opacity': 0.5
+         }
+     })
 }
 
-// Ajoute un événement 'click' sur l'élément HTML avec l'id 'union' pour charger le resultat sur la carte Mapbox
+/**
+ * Cette fonction prend en entrée une FeatureCollection
+ * et renvoie une Feature qui représente la fusion de toutes les Features dans la collection
+ * Elle utilise la fonction Turf.js "dissolve" pour dissoudre les limites entre les features adjacentes.
+ * @param {FeatureCollection} featureCollection - un set de points surlesquels on travaille
+ * @returns {FeatureCollection} Une featureCollection de polygone dissous
+ */
+function unionFeatures(featureCollection) {
+    const mergedFeature = turf.dissolve(featureCollection);
+    return mergedFeature;
+}
+
+        // Ajoute un événement 'click' sur l'élément HTML avec l'id 'dissolve' pour charger le resultat sur la carte maplibre
+document
+    .getElementById('gridder')
+    .addEventListener('click', generateGrid);
+
+// Ajoute un événement 'click' sur l'élément HTML avec l'id 'union' pour charger le resultat sur la carte maplibre
 document
     .getElementById('union')
     .addEventListener('click', union);
+
+    // Ajoute un événement 'click' sur l'élément HTML avec l'id 'dissolve' pour charger le resultat sur la carte maplibre
+document
+    .getElementById('dissolve')
+    .addEventListener('click', dissolver);
+
