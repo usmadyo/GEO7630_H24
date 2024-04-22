39eefbf8a9bbb262ae3ee608a51c72723fee0b61
Youssef SMADI <68562466+usmadyo@users.noreply.github.com>
2024-04-22 04:32:05 +0000

    TP3

--- Atlas/app/Equipe5/code.js
+++ Atlas/app/Equipe5/code.js
@@ -22,22 +22,26 @@ var scale = new maplibregl.ScaleControl({
 });
 map.addControl(scale); // ajout du contrôle en bas à gauche de la carte
 
+// list des couches à supprimr avec le bouton Reset map
 const myLayers = ['stationsRSQA','h2','h4', 'h5', 'h6','h7', 'h8'];
 
+// Compteur de nombre des enregistrements RSQA
 function featureCount() {
     var features = map.queryRenderedFeatures({ layers: ['stationsRSQA'] }); // UTILISER LA FONCTION MaplibreGL pour requeter les elements a l'ecran
     document.getElementById('featureCount').value = features.length;
 }
-
+// Appel de la fonction calculateDistance (turf.js) pour calculer la distance moyenne entre les stations RSQA
 function calculateDistance(){
     featureCollection = ['stationsRSQA'];
     document.getElementById('distanceResult').value = Math.round(averageDistance(randomPoints), 2);
 }
 
+// Appel de la fonction createBuffer (turj.js) pour créer des buffer avec les stations RSQA
 function createBuffer(){
     featureCollection = ['stationsRSQA'];
     console.log(radiusInput)
 }
 
+// Appeler la fonction pour créer une couche 2.5D
 function generate3D(){
 }
